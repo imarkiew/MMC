c55724763dcd7b00615d3d81a1d9067fe2085b01
Igor Markiewicz <igomar067@gmail.com>
2018-04-16 15:19:20 +0200

    fmode changed to %

--- lab_1/zad_1/zad_1.c
+++ lab_1/zad_1/zad_1.c
@@ -5,11 +5,11 @@
 int main()
 {
     int n = 2500;
-    double m = pow(2,13)-1;
-    double a_1 = 17;
-    double c = 0;
-    double x_0 = 19;
-    double x_1 = 0;
+    int m = (int)pow(2,13)-1;
+    int a_1 = 17;
+    int c = 0;
+    int x_0 = 19;
+    int x_1 = 0;
 
     FILE *file = fopen("zad_1_a=17.txt", "w");
     if (file == NULL)
@@ -20,8 +20,8 @@ int main()
 
     for(int i = 0; i < n; i++)
     {
-        x_1 = fmod(a_1*x_0 + c, m);
-        fprintf(file, "%.3g %.3g\n", x_1, x_0);
+        x_1 = (a_1*x_0 + c) % m;
+        fprintf(file, "%d %d \n", x_1, x_0);
         x_0 = x_1;
     }
     fclose(file);
@@ -30,12 +30,12 @@ int main()
 
     a_1 = 85;
     x_0 = 19;
-    double values[n];
+    int values[n];
     values[0] = x_0;
 
     for(int i = 1; i < n; i++)
     {
-        values[i] = fmod(a_1* values[i - 1] + c, m);
+        values[i] = (a_1* values[i - 1] + c) % m;
     }
 
     FILE *file_2 = fopen("zad_1_b.txt", "w");
@@ -46,7 +46,7 @@ int main()
     }
     for(int i = 0; i < n - 1; i++)
     {
-        fprintf(file_2, "%.3g %.3g\n", values[i], values[i + 2]);
+        fprintf(file_2, "%d %d\n", values[i], values[i + 2]);
     }
     fclose(file_2);
 
